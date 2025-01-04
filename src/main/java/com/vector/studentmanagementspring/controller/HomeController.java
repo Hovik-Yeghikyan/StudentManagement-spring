96289ec094cc4fec6d1c4a20cc8e34add63443cf
Hovik Yeghikyan <hovikyeghikyan@gmail.com>
2025-01-05 02:15:59 +0300

    added ImageUploadController

--- src/main/java/com/vector/studentmanagementspring/controller/HomeController.java
+++ src/main/java/com/vector/studentmanagementspring/controller/HomeController.java
@@ -1,37 +1,15 @@
 package com.vector.studentmanagementspring.controller;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 @Controller
 public class HomeController {
-    @Value("${studentManagement.upload.path}")
-    private String uploadPath;
-
 
     @GetMapping("/")
     public String homePage() {
         return "home";
     }
 
-    @GetMapping(value = "/getImage", produces = MediaType.IMAGE_JPEG_VALUE)
-    public @ResponseBody byte[] getImage(@RequestParam("imageName") String imageName) throws IOException {
-        File file = new File(uploadPath + imageName);
-        if (file.exists()) {
-            try (InputStream in = new FileInputStream(file)) {
-                return IOUtils.toByteArray(in);
-            }
-        }
-        return null;
-    }
+
 }
